f8a58703156b74afa5322c8bb93eb64ec855ce78
Raussky <rauangadil5@gmail.com>
2024-05-19 22:54:09 +0500

    Initial Commit

---
+++ ArbuzClone/ArbuzCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArbuzCloneApp.swift
+//  ArbuzClone
+//
+//  Created by Rauan on 19.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct ArbuzCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
